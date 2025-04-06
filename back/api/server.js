130ac2513c991de22d0c1e640e8f25e879b1dcf9
eloflo41 <elo.robert41@gmail.com>
2025-04-06 20:18:41 +0200

    Ajout du front et back, fichiers HTML conservés

---
+++ back/api/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const firebaseAdmin = require('firebase-admin');
+const path = require('path');
+
+
+const app = express();
+const port = 3000;
+
+// Initialise Firebase Admin SDK
+const serviceAccount = require('./serviceA.json'); // Télécharge la clé depuis la console Firebase
+
+firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(serviceAccount),
+    databaseURL: "https://ton-projet.firebaseio.com" // Remplace par ton URL Firebase
+});
+
+// Exemple d'authentification utilisateur (vérifier le token)
+app.post('/verifyToken', (req, res) => {
+    const idToken = req.body.token; // Assure-toi que le token est envoyé dans le body
+
+    firebaseAdmin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            const uid = decodedToken.uid;
+            res.json({ message: "Utilisateur vérifié", uid });
+        })
+        .catch(error => {
+            res.status(401).json({ error: "Token invalide" });
+        });
+});
+
+app.use(express.static(path.join(__dirname, '../../front')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../front/index.html'));
+  });
+  
+
+app.listen(port, () => {
+    console.log(`Le serveur écoute sur le port ${port}`);
+});
